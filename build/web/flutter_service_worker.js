6208622067df4635bec4018ecf94d23d4fca1341
Marcos Vinicius <marcosvinicus444@gmail.com>
2025-06-17 16:53:18 -0300

    Nova build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1a5efd0ac635702f6b54fe1bf2985c1a
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "0b4dd465b84470b2b869f0acce20969e",
+"flutter_bootstrap.js": "40e9f261a43c5c0f4174acdffb363b44",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
