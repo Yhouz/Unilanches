d4615f210b9ba155d52a7ec9bda449349758f8b1
Marcos Vinicius <marcosvinicus444@gmail.com>
2025-06-23 17:45:46 -0300

    Nova build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4025480853"
+    serviceWorkerVersion: "2192816541"
   }
 });
